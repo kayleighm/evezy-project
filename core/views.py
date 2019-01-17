6fa431ca9e267b3e5f2b67b5990210ac18365958
Kayleigh Maynard <kayleigh@commissioncrowd.com>
2019-01-17 11:38:29 +0000

    Authentication views

--- core/views.py
+++ core/views.py
@@ -1,6 +1,12 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.edit import FormView
 
 # Create your views here.
+class SignUpView(FormView):
+    template_name = 'registration/signup.html'
+    form_class = UserCreationForm
+    success_url = '/login/'
+
+    def form_valid(self, form):
+        form.save()
+        return super(SignUpView, self).form_valid(form)
